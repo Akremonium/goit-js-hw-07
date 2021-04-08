6604ca36dcb5dfaa296bbcacfdc8b829a2c3d26f
Akremonium <akremonium@gmail.com>
2021-04-08 20:10:27 +0300

    task-01-refactoring

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,25 @@
-const categoriesEl = document.querySelector("#categories");
+// const categoriesEl = document.querySelector("#categories");
+// const allItemsEl = categoriesEl.querySelectorAll(".item");
 
-console.log(`В списке ${categoriesEl.children.length} категории`);
+// console.log(`В списке ${allItemsEl.length} категории`);
 
-const allItemsEl = categoriesEl.querySelectorAll(".item");
+// const list = (item) => {
+//   console.log(`Категория: ${item.firstElementChild.textContent}`);
+//   console.log(
+//     `Количество элементов: ${
+//       item.lastElementChild.querySelectorAll("li").length
+//     }`
+//   );
+// };
 
-const list = (item) => {
-  console.log(
-    ` Категория: ${item.firstElementChild.textContent} 
-Количество элементов: ${item.lastElementChild.querySelectorAll("li").length}`
-  );
-};
+// list(allItemsEl[0]);
+// list(allItemsEl[1]);
+// list(allItemsEl[2]);
 
-list(allItemsEl[0]);
-list(allItemsEl[1]);
-list(allItemsEl[2]);
+const allItemsEl = document.querySelectorAll(".item");
+console.log(`В списке ${allItemsEl.length} категории.`);
+
+allItemsEl.forEach((category) => {
+  console.log(`Категория: ${category.children[0].textContent}`);
+  console.log(`Количество элементов: ${category.children[1].children.length}`);
+});
